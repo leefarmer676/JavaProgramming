e576e84aeb69bf724d07031e3958ec9dccd7a21e
leefarmer676 <lee.farmer0208@gmail.com>
2022-01-14 08:47:22 -0700

    change 2

--- src/Day38_Inheritance/CarTask/Tesla.java
+++ src/Day38_Inheritance/CarTask/Tesla.java
@@ -1,7 +1,12 @@
 package Day38_Inheritance.CarTask;
 
-public class Tesla{
+public class Tesla extends Car{
 
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
 
-
+    public void autoPilot(){
+        System.out.println(brand + " " + model + " is in autopilot mode");
+    }
 }
