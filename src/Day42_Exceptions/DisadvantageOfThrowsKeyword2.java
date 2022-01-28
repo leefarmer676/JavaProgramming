8a3dc053d417a5772896d27cf142b02655b9867c
leefarmer676 <lee.farmer0208@gmail.com>
2022-01-28 09:04:27 -0700

    disadvantage 2

--- src/Day42_Exceptions/DisadvantageOfThrowsKeyword2.java
+++ src/Day42_Exceptions/DisadvantageOfThrowsKeyword2.java
@@ -4,11 +4,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class DisadvantageOfThrowsKeyword2 {
-    public static void method1() throws FileNotFoundException{
-        new FileInputStream("");
+
+    public static void main(String[] args) throws InterruptedException{
+        method1();//line above - always use the class names compiler tells you, instead of parent class, exception
+        method2();
+        method3();
+
+        String str = null;
+        try {
+            System.out.println(str.charAt(0));//unchecked -
+        }catch(RuntimeException e){
+            e.printStackTrace();
+        }
+    }
+    public static void method1() throws InterruptedException{
+        //new FileInputStream("");
     }
 
-    public  static void method2() throws FileNotFoundException{
+    public  static void method2() throws InterruptedException{
         method1();
     }
 
