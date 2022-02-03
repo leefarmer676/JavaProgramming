b2d4d1e87a8a04c1c0c7e6bce186c511bf443157
leefarmer676 <lee.farmer0208@gmail.com>
2022-02-03 08:20:13 -0700

    employee task and all classes created

--- src/Day42_Exceptions/CustomExceptions.java
+++ src/Day42_Exceptions/CustomExceptions.java
@@ -1,4 +1,29 @@
 package Day42_Exceptions;
 
+class FadyException extends RuntimeException{//custom unchecked exception
+    public FadyException(){
+        super("Time for a short break");
+    }
+
+    public FadyException(String message){
+        super(message);
+    }
+
+}
+
+class NoBreakException extends Exception{ //custom checekd exception
+
+}
+
 public class CustomExceptions {
+    //custom unchecked exception
+    public static void main(String[] args) {
+        //throw new FadyException("It's time for lunch");//only doesn't have error bc extends RubTimeException
+
+        try {
+            throw new NoBreakException(); //error bc this is a checked exception
+        } catch (NoBreakException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/day01_JavaPractice/Quiz1.java
+++ src/day01_JavaPractice/Quiz1.java
@@ -1,18 +1,26 @@
 package day01_JavaPractice;
 
+
 import java.util.Arrays;
 
 public class Quiz1{
-    public static int minNum(int [] nums){
-        Arrays.sort(nums);
-        return nums[0];
-    }
+   static int a = 50;
 
     public static void main(String[] args) {
-        int [] arr = {4,1,0,8,-9,3,4};
-        int min = minNum(arr);
-    }
+        a = 100;
 
+        try{
+            a = 200;
+        }catch(Exception e){
+            a = 300;
+        }finally{
+            a=400;
+        }
+        System.out.println(a);
+    }
+static{
+        a = 500;
+}
 }
 
 
