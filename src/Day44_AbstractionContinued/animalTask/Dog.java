99fa723c4aeafbfd0c38ecaf0977fbbfb94de439
leefarmer676 <lee.farmer0208@gmail.com>
2022-02-04 11:24:52 -0700

    Day44 Animal Subclasses created

---
+++ src/Day44_AbstractionContinued/animalTask/Dog.java
@@ -0,0 +1,22 @@
+package Day44_AbstractionContinued.animalTask;
+
+public final class Dog extends Animal{
+    //if parent class has a constructor, child class must call it
+    public Dog(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+    //unique method for dog class - added before extending animal
+    //cannot inherit "name" because it's private; must use getName to use private field
+    public void bark(){
+        System.out.println(getName()+ " is barking");
+    }
+    //override abstract method from parent class - access modifier same or more visible,
+    //return type same, parameters same, etc.
+    @Override
+    public void eat() {
+        System.out.println(getName()+ " is eating pizza");
+    }
+
+
+
+}
